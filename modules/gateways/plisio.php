1794f16b7adbe7fe6fa01acc82337c486298491f
plisdeveloper <plugins@plisio.net>
2022-08-08 23:05:34 +0300

    Credit system support added

--- modules/gateways/plisio.php
+++ modules/gateways/plisio.php
@@ -23,25 +23,12 @@ function plisio_config()
     );
 }
 
-function get_plisio_receive_currencies ($source_currency, $api_key) {
-    $client = new PlisioClient($api_key);
-    $currencies = $client->getCurrencies($source_currency);
-    return array_reduce($currencies, function ($acc, $curr) {
-        $acc[$curr['cid']] = $curr;
-        return $acc;
-    }, []);
-}
-
 function plisio_createOrder($params)
 {
     $client = new PlisioClient($params['ApiAuthToken']);
     $returnLink = trim($params['systemurl'], "/");
 
-    $plisio_receive_currencies = get_plisio_receive_currencies($params['currency'], $params['ApiAuthToken']);
-    $plisio_receive_cids = array_keys($plisio_receive_currencies);
-
     $data = array(
-        'currency' => $plisio_receive_cids[0],
         'order_name' => $params['companyname'] . ' Order #' . $params['invoiceid'],
         'order_number' => $params['invoiceid'],
         'description' => $params['description'],
@@ -75,7 +62,7 @@ function plisio_link($params)
     if (false === isset($params) || true === empty($params)) {
         die('[ERROR] In modules/gateways/plisio.php::plisio_link() function: Missing $params data.');
     }
-    if ((isset($_POST) && !empty($_POST) && isset($_POST['sbmt'])) or $_GET['a'] == 'complete') {
+    if ((isset($_POST) && !empty($_POST) && isset($_POST['sbmt'])) or ($_GET['a'] == 'complete') or ($_GET['action'] == 'addfunds')) {
         return plisio_createOrder($params);
     } else {
         $form = '<form method="POST">';
